58712bf849f0a9c66575876a62b03004b769bfd9
Jonathan Willis <Jonathan.Willis@vu.com>
2023-02-17 21:06:45 -0600

    4.9.2 Monkeys Control Our Day

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -3,7 +3,12 @@ def calculateTime():
     # This first line is provided for you
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
-
+    if monkey_one == str('y') and monkey_two == str('y'):
+        print("Uh Oh! We're in trouble!")
+    elif monkey_one == str('n') and monkey_two == str('n'):
+        print("Uh Oh! We're in trouble!")
+    else:
+        print("Yay! We're going to have a good day!")
     # end assignment
 
 
